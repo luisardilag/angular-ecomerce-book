d9b56aa7562d2092ef2cbb026174a6821bfcbd95
Luis Ardila <luisardilag@gmail.com>
2025-11-19 22:55:35 -0300

    constructor

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,17 +1,22 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, ViewEncapsulation } from '@angular/core';
 import { Product } from '../product';
 
 @Component({
   selector: 'app-product-detail',
   imports: [],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class ProductDetailComponent {
   product = input<Product>();
   added = output<Product>();
 
-  addToCart(){
+  addToCart() {
     this.added.emit(this.product()!);
   };
+
+  get productTitle() {
+    return this.product()?.title;
+  }
 }
